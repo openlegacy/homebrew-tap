12e8b32831ab21ea6df4e14e79792a6b46198aff
elinorschwarzblat <elinor.schwarzblat@gmail.com>
2020-06-02 17:33:45 +0300

    change .ol-completion.sh to ol-completion.sh

--- bin/addCompletionSupportt.sh
+++ bin/addCompletionSupportt.sh
@@ -6,7 +6,7 @@ echo "Installing 1...${SHELL_TYPE}"
 
 if [[ $SHELL_TYPE =~ $pattern ]]; then
   echo "Installing 2...${SHELL_TYPE}"
-	echo "source ##PREFIX##/bin/.ol-completion.sh" >> ~/.bash_profile
+	echo "source ##PREFIX##/bin/ol-completion.sh" >> ~/.bash_profile
 	source ~/.bash_profile
 fi
 
