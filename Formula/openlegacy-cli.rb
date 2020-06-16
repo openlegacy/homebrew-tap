d3456aaf718f15aa1e78b960a7a8277ab89970f1
Maor ZIlberg <maor.z@openlegacy.com>
2020-06-16 18:53:58 +0300

    fix tap structure

---
+++ Formula/openlegacy-cli.rb
@@ -0,0 +1,20 @@
+class OpenlegacyCli < Formula
+    desc "Digitize Your Legacy With OpenLegacy's Hub"
+    homepage "https://github.com/openlegacy/homebrew-openlegacy-cli"
+    url "https://github.com/openlegacy/homebrew-openlegacy-cli/archive/v1.2.092.tar.gz"
+    version "1.3.0"
+    sha256 "fd3db716c701f3b27836f4224c0bba8703d559ddf4646af9fcadbf73ba80df2a"
+  
+    def install
+      chmod 0755, "ol"
+      inreplace "ol", "##PREFIX##", "#{prefix}"
+      prefix.install "cli.jar"
+      bin.install "ol"
+      bash_completion.install "ol-completion.bash" => "ol"
+    end
+  
+    test do
+      assert_match "-F _ol",
+      shell_output("bash -c 'source #{bash_completion}/ol && complete -p ol'")
+    end
+  end
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# homebrew-openlegacy-cli
+# Git@github.com:openlegacy Tap.git
+
+## How do I install these formulae?
+`brew install git@github.com:openlegacy/tap.git/<formula>`
+
+Or `brew tap git@github.com:openlegacy/tap.git` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
