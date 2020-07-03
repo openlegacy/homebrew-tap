56e3eda7765596fffb0d39cdd9cfa536fbec716c
yanp <yanp@example.com>
2020-07-03 14:46:55 +0300

    DEVOPS-237 - upd readme - upd formula - add template & render for Formula

--- Formula/openlegacy-cli.rb
+++ Formula/openlegacy-cli.rb
@@ -1,20 +1,19 @@
 class OpenlegacyCli < Formula
-    desc "Digitize Your Legacy With OpenLegacy's Hub"
-    homepage "https://github.com/openlegacy/homebrew-openlegacy-cli"
-    url "https://ol-public-artifacts.s3.amazonaws.com/cli/1.3.0/prod/387/openlegacy_cli_linux.tar.gz"
-    version "1.3.0"
-    sha256 "3d87fa997ae0ccf4000dcd2dd3c085a3d18fdb41cbb57110292aa1ee6936eec0"
-  
-    def install
-      chmod 0755, "ol"
-      inreplace "ol", "##PREFIX##", "#{prefix}"
-      prefix.install "cli.jar"
-      bin.install "ol"
-      bash_completion.install ".ol-completion.sh" => "ol"
-    end
-  
-    test do
-      assert_match "-F _ol",
-      shell_output("bash -c 'source #{bash_completion}/ol && complete -p ol'")
-    end
+  desc "Digitize Your Legacy With OpenLegacy's Hub"
+  homepage "https://github.com/openlegacy/homebrew-openlegacy-cli"
+  url "https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.5.0/openlegacy-cli.zip"
+  version "1.5.0"
+  sha256 "b48a5553ab3b4d539439ed66e4a362adb86baf72b59a39c870517e379de98b3d"
+
+  def install
+    inreplace "ol/bin/ol", "##PREFIX##", "#{prefix}"
+    prefix.install "ol/bin/cli.jar"
+    bin.install "ol/bin/ol"
+    bash_completion.install "ol/bin/.ol-completion.sh" => "ol"
+    zsh_completion.install "ol/bin/.ol-completion.zsh" => "_ol"
   end
+
+  test do
+    system bin/"ol", "-v"
+  end
+end
