c640fe258dd4d69acc5723601107e4368b2e87a7
ol-test <admin@openlegacy.com>
2020-09-21 10:00:07 +0000

    Update to 1.10.0

--- Formula/openlegacy-cli.rb
+++ Formula/openlegacy-cli.rb
@@ -1,9 +1,9 @@
 class OpenlegacyCli < Formula
   desc "Digitize Your Legacy With OpenLegacy's Hub"
   homepage "https://github.com/openlegacy/openlegacy-cli"
-  url "https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.9.0/linux-macos/openlegacy-cli.zip"
-  version "1.9.0"
-  sha256 "e3a17ab922ceb38ceaf491cac50bb40d71d9ee48cefadef223d010b4be7de9ab"
+  url "https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.10.0/linux-macos/openlegacy-cli.zip"
+  version "1.10.0"
+  sha256 "b5c63e316377c0a917294eb56002685a9dcb002f89a0e8e3b21c724ebc913e77"
 
   def install
     inreplace "bin/ol", "##PREFIX##", "#{prefix}"
