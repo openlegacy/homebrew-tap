fbffc73af5923164007e9d629afbc736037add08
ol-test <admin@openlegacy.com>
2021-01-26 09:16:36 +0000

    Update: 1.19.0

---
+++ Formula/openlegacy-cli.rb
@@ -0,0 +1,29 @@
+class OpenlegacyCli < Formula
+  desc "Digitize Your Legacy With OpenLegacy's Hub"
+  homepage "https://github.com/openlegacy/openlegacy-cli"
+  url "https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.19.0/linux-macos/openlegacy-cli.zip"
+  version "1.19.0"
+  sha256 "f853ab8594abc135ceba1f963e044d193156541f4b7e2215d442831363732f18"
+
+  def colorize(text, color_code)
+    "\e[#{color_code}m#{text}\e[0m"
+  end
+
+  def green(text); colorize(text, 32); end
+
+  def install
+    inreplace "bin/ol", "##PREFIX##", "#{prefix}"
+    prefix.install "bin/cli.jar"
+    bin.install "bin/ol"
+    bash_completion.install "bin/.ol-completion.sh" => "ol"
+    zsh_completion.install "bin/.ol-completion.zsh" => "_ol"
+
+    puts green('*********************************************************************************************')
+    puts 'Note that the CLI require java 11, please make sure its installed and JAVA_HOME is configured'
+    puts green('*********************************************************************************************')
+  end
+
+  test do
+    system bin/"ol", "-v"
+  end
+end
