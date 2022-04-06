0ac519ca63d41950fde28e1df890e2dfd8693e80
ol-test <admin@openlegacy.com>
2022-04-06 14:38:48 +0000

    Update: 1.49.2::alpha::1324

--- Formula/openlegacy-cli-alpha.rb
+++ Formula/openlegacy-cli-alpha.rb
@@ -3,7 +3,7 @@ class OpenlegacyCliAlpha < Formula
   homepage "https://github.com/openlegacy/openlegacy-cli"
   url "https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/alpha/linux-macos/openlegacy-cli.zip"
   version "alpha"
-  sha256 "cc6fada2ce369d0990bd14b5e229f5028d08c17d379fa4a377cb1ac28c481034"
+  sha256 "ef34ed8ad5360faeb69aa8e93f72349f98417612b405bc0cb2a3014ad92ac253"
 
   def colorize(text, color_code)
     "\e[#{color_code}m#{text}\e[0m"
