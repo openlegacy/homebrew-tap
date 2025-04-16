69d8a1ba62f5dbb0a0fc4e5d0ef97249e2583f10
ol-test <admin@openlegacy.com>
2025-04-16 13:31:17 +0000

    Update: 3.0.9.1

--- Formula/openlegacy-cli.rb
+++ Formula/openlegacy-cli.rb
@@ -1,9 +1,9 @@
 class OpenlegacyCli < Formula
   desc "Digitize Your Legacy With OpenLegacy's Hub"
   homepage "https://github.com/openlegacy/openlegacy-cli"
-  url "https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/3.0.9/linux-macos/openlegacy-cli.zip"
-  version "3.0.9"
-  sha256 "06d1ba59ab06c6ecdbe3dcc06c174f79ccd0137c3c397487a0eafb1a60d9f60c"
+  url "https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/3.0.9.1/linux-macos/openlegacy-cli.zip"
+  version "3.0.9.1"
+  sha256 "d60e8e068810cc36808a5978d33c7665da396d5aa1e91fd8ef76e5f0319c3f4a"
 
   def colorize(text, color_code)
     "\e[#{color_code}m#{text}\e[0m"
